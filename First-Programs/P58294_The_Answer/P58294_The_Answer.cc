0abd50469fc06bd3fdb6a3f88ab932ef49018b32
Edhey <alu0101552392@ull.edu.es>
2023-01-27 13:56:45 +0000

    P58294 commit 1

---
+++ First-Programs/P58294_The_Answer/P58294_The_Answer.cc
@@ -0,0 +1,19 @@
+/**
+  * Universidad de La Laguna
+  * Escuela Superior de Ingeniería y Tecnología
+  * Grado en Ingeniería Informática
+  * Informática Básica 2022-2023
+  *
+  * @file P57548_Sum_of_two_integer_numbers.cc
+  * @author Himar Edhey Hernández Alonso alu0101552392@ull.edu.es
+  * @date Oct 31 2022
+  * @brief programa que lee dos números e imprime su suma 
+  * @bug No hay errores conocidos
+  * @see https://jutge.org/problems/P57548_en
+  */
+
+#include<iostream>
+
+int main() {
+  std::cout << "42";
+}
