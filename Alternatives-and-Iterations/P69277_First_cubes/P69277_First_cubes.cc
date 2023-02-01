25c8af949b85c9a0d001995e418871689d1da982
Edhey <alu0101552392@ull.edu.es>
2023-02-01 11:33:09 +0000

    Arreglo en nombres de archivos y coomit 1 P69277

---
+++ Alternatives-and-Iterations/P69277_First_cubes/P69277_First_cubes.cc
@@ -0,0 +1,40 @@
+/**
+  * Universidad de La Laguna
+  * Escuela Superior de Ingeniería y Tecnología
+  * Grado en Ingeniería Informática
+  * Informática Básica 2022-2023
+  *
+  * @author Himar Edhey Hernández Alonso alu0101552392@ull.edu.es
+  * @date Jan 30 2023
+  * @brief Programa que lea un número n, e imprima 0^3, 1^3, ..., (n-1)^3,n^3.
+  * @bug No hay errores conocidos
+  * @see https://jutge.org/problems/P69277_en
+  */
+
+#include <iostream>
+
+/** Función que imprime la introduccion del programa
+  *
+  * @return Texto con el resumen de la utilidad del programa y como ejecutarlo
+  */
+void Introduccion() {
+  std::cout << "Programa que lea un número n, e imprima 0^3, 1^3, ..., (n-1)^3,"
+  << "n^3.";
+  std::cout <<  std::endl;
+  std::cout <<  "Escriba un número: "; 
+  std::cout << std::endl;
+}
+
+void SucesionDePotenciasDe3(const int numero){
+  for (int i{0}; i < numero; i++) {
+    std::cout << i * i * i << ",";
+  }
+  std::cout << numero * numero * numero << std::endl;
+}
+
+int main() {
+  Introduccion();
+  int numero{};
+  std::cin >> numero;
+  SucesionDePotenciasDe3(numero);
+}
